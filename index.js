1ceb87c0c810f5568826ac979bce83b1382387b4
Adarsh <ar1500@srmist.edu.in>
2024-08-02 19:43:17 +0530

    minor fix

--- index.js
+++ index.js
@@ -4,7 +4,11 @@ const bodyParser = require('body-parser');
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.use(cors());
+app.use(cors(
+   {
+    origin: "https://bajaj-frontend-alpha.vercel.app"
+   }
+));
 
 // Middleware
 app.use(bodyParser.json());
